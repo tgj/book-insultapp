be634374c5493c04ec94f416fbab86bcfa2b5ed4
Tyler Johnston <tylergarrettjohnston@gmail.com>
2018-03-03 05:19:18 -0500

    Reference database for insults, rather than using mock data

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,19 +1,52 @@
 package org.openshift;
 
-import java.util.Random;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class InsultGenerator {
-	public String generateInsult() {
-		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
-		String vowels = "AEIOU";
-		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
-		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
-	}
+
+    public String generateInsult() {
+        final String vowels = "AEIOU";
+        String article = "an";
+        String insult = "";
+
+        try {
+            String databaseURL = "jdbc:postgresql://";
+            databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+            databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+
+            String username = System.getenv("POSTGRESQL_USER");
+            String password = System.getenv("PGPASSWORD");
+            Connection connection = DriverManager.getConnection(databaseURL, username,
+                    password);
+
+            if (connection != null) {
+                final String SQL = "SELECT a.string AS first, b.string AS second, c.string AS noun  from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1";
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(SQL);
+
+                while (resultSet.next()) {
+                    if (vowels.indexOf(resultSet.getString("first").charAt(0)) == -1) {
+                        article = "a";
+                    }
+
+                    insult = String.format("Thou art %s %s %s %s!",
+                        article,
+                        resultSet.getString("first"),
+                        resultSet.getString("second"),
+                        resultSet.getString("noun"));
+                }
+
+                resultSet.close();
+                connection.close();
+            }
+        } catch (Exception e) {
+            return "An error has occurred trying to connect to the database.";
+        }
+
+        return insult;
+    }
 
 }
